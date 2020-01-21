5220bb474bdd773fdd0561fceef38f42c8d37b52
Jazlyn Ang <angjazlyn@hotmail.com>
2020-01-21 17:20:42 +0800

    add benchmark

--- sparse_binary/sparse_binary_test.go
+++ sparse_binary/sparse_binary_test.go
@@ -4,18 +4,28 @@ import (
 	"testing"
 )
 
-func TestGetSparseNumber(t *testing.T) {
-	// todo add more test cases
-	expected := map[int]int{
-		21: 21,
-		22: 32,
-	}
+// todo add more test cases
+var expected = map[int]int{
+	21: 21,
+	22: 32,
+}
 
+func TestGetSparseNumber(t *testing.T) {
 	for input, output := range expected {
 		answer := GetSparseNumber(input)
 
 		if answer != output {
 			t.Errorf("Input: %d, Expected Output: %d\n, Actual Output: %d", input, output, answer)
+		}
+	}
+}
+
+func BenchmarkGetSparseNumber(b *testing.B) {
+	// todo write benchmark
+
+	for i := 0; i < b.N; i++ {
+		for input := range expected {
+			GetSparseNumber(input)
 		}
 	}
 }
