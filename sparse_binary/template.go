8b7318260c8cbb9eb1845d8c5d687606c9db7c7f
Jazlyn Ang <angjazlyn@hotmail.com>
2020-01-21 17:18:13 +0800

    comment

--- sparse_binary/template.go
+++ sparse_binary/template.go
@@ -1,10 +1,14 @@
 package sparse_binary
 
-//We say a number is sparse if there are no adjacent ones in its binary representation.
-//For example, 21 (10101) is sparse, but 22 (10110) is not.
-//For a given input N, find the smallest sparse number greater than or equal to N.
+// This problem was asked by Oracle.
 //
-//Do this in faster than O(N log N) time.
+// We say a number is sparse if there are no adjacent ones in its binary representation.
+// For example, 21 (10101) is sparse, but 22 (10110) is not.
+// For a given input N, find the smallest sparse number greater than or equal to N.
+//
+// Do this in faster than O(N log N) time.
+//
+// Source: https://www.dailycodingproblem.com/
 
 func _GetSparseNumber(n int) int {
 
